7ca9359917647c0858c875ebc09004e202638f2f
Lauri Ojansivu <x@xet7.org>
2024-12-06 09:44:58 +0200

    Updates

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ export MONGO_URL=mongodb://127.0.0.1:27017/wekan
 export ROOT_URL=http://localhost:2000
 export PORT=2000
 
-cd bundle
+cd build/bundle
 echo "Starting to http://localhost:2000"
 node main.js
 cd ..
