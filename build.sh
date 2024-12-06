172f1106f4751757d4a0ec602fe402ac71ee4133
Lauri Ojansivu <x@xet7.org>
2024-12-06 10:32:29 +0200

    MongoDB install info etc.

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 time (meteor npm install --omit=dev && \
-meteor build build --directory --platforms=web.browser)
+meteor build build --directory --platforms=web.browser &&
+cd build/bundle/programs/server && npm install
+)
 
 echo "Bundle built to directory build"
