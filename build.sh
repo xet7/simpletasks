77a0dfe742689c85efa7d4b6999b88048be57f7f
Lauri Ojansivu <x@xet7.org>
2024-12-06 10:41:20 +0200

    Updates.

--- build.sh
+++ build.sh
@@ -5,4 +5,11 @@ meteor build build --directory --platforms=web.browser &&
 cd build/bundle/programs/server && npm install
 )
 
+# Because there was error that is at simple-error.txt ,
+# it was needed to change to directory build/bundle/programs/server
+# and do npm install, like at above.
+
+# Amount of time used is at simple-buildtime.txt
+# when used at RasPi4 to build bundle to directory build/bundle.
+
 echo "Bundle built to directory build"
